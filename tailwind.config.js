6b274a53d0dd050eae3314d9d9025003bef3f77f
Aarón Martín <aamarber@gmail.com>
2023-10-30 11:55:28 +0100

    Flex+tailwind solution

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    'index.html'
+    './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      margin:{
+        'full': '100%'
+      }
+    },
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000000',
+      'darkgray': '#5E6472',
+      'subheader': '#AED9E0',
+      'login': '#FAF3DD',
+      'maincontent': '#FFA69E',
+      'secondarycontent': '#B8F2E6'
+    }
   },
   plugins: [],
 }
